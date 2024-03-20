66f39ad1ce695a9e4eefabcce556fb7582e2f2c8
vicky wong <vicky.boci@gmail.com>
2024-03-21 00:20:12 +0800

    jkjk

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'',
+  
+  command: 'CI =npm run build',
+  publish:'build',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.js',
   }
+  
+
 })
